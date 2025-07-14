accc8c5c9163e20b27a2e045d78b4501a3487b6b
ltarparasneha <tarparasneha@gmail.com>
2025-07-14 09:25:36 +0530

    odd even program

---
+++ index.js
@@ -0,0 +1,23 @@
+const name="sneha"
+console.log("hello "+name)
+
+const n=5
+if(n%2)
+{
+    console.log("odd number")
+}
+else
+{
+    console.log("even number")
+}
+
+const a=5
+const b=9
+if(a<b)
+{
+    console.log("max number")
+}
+else
+{
+    console.log("min number")
+}
